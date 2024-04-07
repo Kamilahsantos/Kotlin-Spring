d8b1c6071b645108c4803cf710b8f79fe13ce950
Kamila Santos <kahsantos89@gmail.com>
2024-04-07 10:18:15 -0300

    classes e atributos

--- src/main/kotlin/classe_atributo.kt
+++ src/main/kotlin/classe_atributo.kt
@@ -1,11 +1,20 @@
-class Carro(val cor: String,val  anoFabricacao: Int) {
+class Carro(var cor: String, var anoFabricacao: Int, val dono: Dono) {
 
+//val imutavel nao pode fazer set
+//var pode alterar
+//se nao coloca val ou var não consegue acessar a variavel
 
 
 }
 
-fun main(){
-    var carro = Carro("vermelho", 2024)
-    println(carro.cor)
+data class Dono(var nome: String, var idade: Int) {
+//se coloca como dataclass ele imprime o valor e não a ref ao objeto
+}
 
+fun main() {
+    var carro = Carro("vermelho", 2024, Dono("Kamila", 26))
+    println(carro.cor)
+    carro.cor = "violeta"
+    println(carro.cor)
+    println(carro.dono.nome)
 }
